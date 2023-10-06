51b2e50bfc581fddeda7a3dd6ba85aa4bc097e34
Caleb Gbande <calebkwaku1@gmail.com>
2023-10-07 00:00:47 +0100

    ALX rules

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,8 +3,9 @@
   * main - this is a prinf function
   * Return: 0
   */
+
 int main(void)
 {
- printf("with proper grammar, but the outcome is a piece of art,\n");
- return (0);
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
 }
