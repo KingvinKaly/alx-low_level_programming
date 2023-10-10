a59d540af4a129ff93aa05b6a5477f681d69ab7a
Caleb Gbande <calebkwaku1@gmail.com>
2023-10-10 17:20:02 +0100

    C codes

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * This program is to print alphabet
+ * Return 0
+ */
+
+int main(void)
+{
+	char c;
+	int i = 0;
+
+	while (i <= 9)
+	{
+		for (c='a'; c <= 'z'; c++)
+		{
+			putchar('c');
+		}
+		putchar('\n');
+		i++;
+	}
+	return 0;
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+What you need to learn today is Nested loop, and and be able to explain to someone else
