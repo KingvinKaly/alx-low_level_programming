e76b5d7010d180b7e4d719a3078123fe6d48a8ed
Caleb Gbande <calebkwaku1@gmail.com>
2023-10-13 01:19:42 +0100

    ALX

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,21 +1,24 @@
 #include "main.h"
 
 /**
- * more_numbers - print more numbers
+ * print_line - draws a straight line in the terminal
+ * @n: number of times the character _ should be printed
  */
-
-void more_numbers(void)
+void print_line(int n)
 {
-	int i, j;
+	if (n <= 0)
+	{
+	_putchar('\n');
+	}
+	else
+	{
+	int i;
 
-	for (i = 1; i <= 10; i++)
+	for (i = 1; i <= n; i++)
 	{
-	for (j = 0; j <= 14; j++)
-	{
-	if (j >= 10)
-	_putchar('1');
-	_putchar (j % 10 + '0');
+	_putchar('_');
 	}
 	_putchar('\n');
 	}
+
 }
