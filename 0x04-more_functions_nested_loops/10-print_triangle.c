00f78cdea1f0918090970311899f8102d9254894
Caleb Gbande <calebkwaku1@gmail.com>
2023-10-13 01:23:49 +0100

    ALX

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -1,40 +1,31 @@
 #include "main.h"
-#include <stdio.h>
-
 /**
- * main - prints the numbers from 1 to 100, followed by a new line
- * but for multiples of three prints Fizz instead of the number
- * and for the multiples of five prints Buzz
- * Return: Always 0 (Success)
+ * print_triangle - prints a triangle, followed by a new line
+ * @size: size of the triangle
  */
-int main(void)
+void print_triangle(int size)
 {
-	int i;
-
-	for (i = 1; i <= 100; i++)
+	if (size <= 0)
 	{
-	if (i % 3 == 0 && i % 5 != 0)
-	{
-	printf(" Fizz");
-	}
-	else if (i % 5 == 0 && i % 3 != 0)
-	{
-	printf(" Buzz");
-	}
-	else if (i % 3 == 0 && i % 5 == 0)
-	{
-	printf(" FizzBuzz");
-	}
-	else if (i == 1)
-	{
-	printf("%d", i);
+	_putchar('\n');
 	}
 	else
 	{
-	printf(" %d", i);
-	}
-	}
-	printf("\n");
+	int i, j;
 
-	return (0);
+	for (i = 1; i <= size; i++)
+	{
+	for (j = i; j < size; j++)
+	{
+	_putchar(' ');
+	}
+
+	for (j = 1; j <= i; j++)
+	{
+	_putchar('#');
+	}
+
+	_putchar('\n');
+	}
+	}
 }
