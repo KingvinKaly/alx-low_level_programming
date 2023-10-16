46039ed32f7b63844085d6a6838b15cb590cb906
Caleb Gbande <calebkwaku1@gmail.com>
2023-10-16 23:40:12 +0100

    ALX

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include <main.h>
+
+/**
+ *  swap_int - function name
+ *  @a :this is the first paremeter
+ *  @b :this is the second paremeter
+ */
+
+
+void swap_int(int *a, int *b)
+{
+int c;
+	c = *a;
+	*a = *b;
+	*b = c;
+}
